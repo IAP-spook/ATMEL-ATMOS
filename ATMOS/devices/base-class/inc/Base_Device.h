91693c99298ca67062c819fe991cab74f4d7b43a
baxsmail <anxin.bai@gmail.com>
2016-07-07 17:29:38 -0400

    Comments and Doc for Doxygen added.

--- ATMOS/devices/base-class/inc/Base_Device.h
+++ ATMOS/devices/base-class/inc/Base_Device.h
@@ -10,8 +10,18 @@
 #define BASE_DEVICE_H_
 #include "utilities/inc/common.h"
 
+/** @name Declaration and Typedef of the base struct
+ *
+ */
+///@{
 struct _Base_Device;
 typedef struct _Base_Device BaseDevice;
+///@}
+
+/** @name 'Virtual functions' implementation of the struct
+ *  c's implmentation of virtual function ( interface in Java or C# ) and you shouldn't worry about modifying this for now
+ */
+///@{
 typedef struct _Base_Device_vt
 {
 	void (*VTinit)( BaseDevice* );
@@ -19,16 +29,33 @@ typedef struct _Base_Device_vt
 	int (*reset)( BaseDevice* );
 	int (*getType)( BaseDevice* );
 } Base_Device_FctnTable;
+///@}
 
+
+/** @name Definition of the _Base_Device struct
+ *  _Base_Device acts like a base class in C++ and only contains a struct that wraps bunch of 'virtual functions' in it.
+ */
+///@{
 struct _Base_Device
 {
 	Base_Device_FctnTable *device_vt;
 };
+///@}
 
-void BaseDevice_VTinit( BaseDevice* );
-int BaseDevice_init( BaseDevice* );
-int BaseDevice_reset( BaseDevice* );
-int BaseDevice_getType( BaseDevice* );
+/** @name Declaration of the 'virtual functions'
+ *  Declaration of the 'virtual functions' or said interface within current struct
+ */
+///@{
+void BaseDevice_VTinit( BaseDevice* );/**<@brief VirtualTable Init, which loads all following interfaces into the _Base_Device struct. TRY NOT MODIFY THIS. */
+int BaseDevice_init( BaseDevice* );/**<@brief Interface that is used for initialization for any general device( sensors included ).  */
+int BaseDevice_reset( BaseDevice* );/**<@brief Interface that is used for reset ( most likely re-initialization ) when getting some unexpected error for any general device( sensors included ).  */
+int BaseDevice_getType( BaseDevice* );/**<@brief Interface that is used for getting type for current device( sensors included ), and it is not so useful for now(06/27/2016).  */
+///@}
+
+/** @name Declaration of the new API for this struct
+ *  Not useful for base struct except for testing but it just demonstrates the way to do it with other derivative struct.
+ */
+///@{
 BaseDevice* New_Base_Device();
-
+///@}
 #endif
--- ATMOS/devices/other-device/inc/LoadData_Device.h
+++ ATMOS/devices/other-device/inc/LoadData_Device.h
@@ -3,6 +3,13 @@
  * \brief Declaration of LoadDataDevice struct.
  * Created: 2016/4/21 23:55:29
  *  Author: Anxin Bai
+ *
+ *
+ * The LoadData_Device.h along with LoadData_Device.c serve as virtual device that simulated by the mcu( or cpu ). 
+ * It is responsible to access some limited memory ( currently 07/07/2016 managed by DemoStorage_Device.h/.c and defined in /utilities/inc/data_unit.h ) and mark the memory slot that should be used for current period.
+ * At the beginning of each collecting period the execute function, it accesses the limited memory and move to the next available slot and let cur_data pointer points to it.
+ * So during collecting, all sensors use cur_data pointer to load new data but don't need to worry about where in memory to save it and how to manage the memory.
+ * NOTE at 07/07/2016 : You definitely still need this virtual device in the future. But DemoStorageDevice should be modified to act as MemoryDevice ( a virtual device to help manage memory ).
  */ 
 
 
@@ -13,20 +20,39 @@
 #include "devices/other-device/inc/DemoStorage_Device.h"
 #include "utilities/inc/data_unit.h"
 
-extern DataUnit * cur_data;
-extern tm cur_time;
+/** @name Declaration of the current data unit and the current time stamp
+ *
+ */
+///@{
+extern DataUnit * cur_data;/**<@brief DataUnit that used to hold various sensor collections in this collecting period.  */
+extern tm cur_time;/**<@brief Time stamp that used to mark the timestamp of the start time of this collecting period  */
+///@}
 
+/** @name Declaration and Typedef of this derived struct
+ *
+ */
+///@{
 struct _LoadData_Device;
 typedef struct _LoadData_Device LoadDataDevice;
+///@}
 
 
+/** @name 'Virtual functions' implementation of the struct
+ *  c's implmentation of virtual function ( interface in Java or C# ) and you shouldn't worry about modifying this for now
+ */
+///@{
 typedef struct _LoadData_Device_vmt
 {
 	void (*Device_Tinit)(LoadDataDevice*);
 	int (*Execute)(LoadDataDevice*);
 	int (*Configure)(LoadDataDevice*);
 } LoadData_Device_FctnTable;
+///@}
 
+/** @name 'Virtual functions' implementation of the base struct
+ *  c's implmentation of virtual function ( interface in Java or C# ) and you shouldn't worry about modify this for now.
+ */
+///@{
 typedef struct _LoadData_Device_Abstract_vmt
 {
 	void (*VTinit)( LoadDataDevice* );
@@ -34,24 +60,46 @@ typedef struct _LoadData_Device_Abstract_vmt
 	int (*reset)( LoadDataDevice* );
 	int (*getType)( LoadDataDevice* );
 } LoadData_Device_Abstract_FctnTable;
+///@}
 
+/** @name Definition of the _LoadData_Device struct
+ *  _LoadData_Device acts like a derivative class in C++ which derives something from BaseDevice and have some properties of the non-sensor device. Notice it has a My_Device_FctnTable struct pointer wrapping bunch of 'virtual functions' for derivative devices.
+ */
+///@{
 struct _LoadData_Device
 {
 	BaseDevice abstract;
 	LoadData_Device_FctnTable *vmt;
 	int info;
-	DemoStorageDevice *store_device;
+	DemoStorageDevice *store_device;/**<@brief This DemoStorageDevice ( or called MemoryDevice later is needed since this class is only responsible for accessing certain memory and get a proper one to load data but not for managing the memory.)  */
 };
+///@}
 
 
+/** @name Declaration of the 'virtual functions' offered in base sensor struct 
+ *  Declaration of the 'virtual functions' or said interface within base device struct
+ */
+///@{
 void LoadData_Device_VTinit( LoadDataDevice* );
 int LoadData_Device_init( LoadDataDevice* );
 int LoadData_Device_reset( LoadDataDevice* );
 int LoadData_Device_getType( LoadDataDevice* );
+///@}
 
+/** @name Declaration of the new API for this struct
+ *  It is used for instantiate the device in the system. You can adjust the input parameter as long as you use it the same way as you define it.
+ *  Note you need to pass in a pointer of DemoStorageDevice ( or MemoryDevice in the future ) since this class is only responsible for accessing certain memory and get a proper one to load data but not for managing the memory.
+ */
+///@{
 LoadDataDevice* New_LoadData_Device( int num, DemoStorageDevice * ptr );
+///@}
 
+/** @name Declaration of the virtual APIs only used for device ( not for sensors )
+ *  It is used for instantiate the device in the system. You can adjust the input parameter as long as you use it the same way as you define it.
+ */
+///@{
 void LoadData_Device_Tinit(LoadDataDevice *this);
 int LoadData_Device_Execute(LoadDataDevice *this);
 int LoadData_Device_Configure(LoadDataDevice *this);
+///@}
 #endif
